703aab4a45d9ab0aed3760c95b99566331d7c0ab
Charlotte van Waes <105546462+Crmm2000@users.noreply.github.com>
2025-07-01 22:20:33 +0200

    Cleanup Engine.py

--- charlottevanwaes/Engine.py
+++ charlottevanwaes/Engine.py
@@ -1,10 +1,8 @@
 #import packages
 import numpy as np
 import math
-
-#etc
 from tqdm import tqdm
-import utils as u #contains helper functions like periodic boundary condition, experiment details
+import utils as u 
 
 # Main simulation loop
 def simulate_system(radius_repulsive_potential, n_particles, v_0, curvity, persistence_length=5, t_end = 100, dt = 1e-2, boundary = 20, stiffness = 1, interacting = False, mode = None, N = 2, mode_particles = 'ABP'):
@@ -12,7 +10,7 @@ def simulate_system(radius_repulsive_potential, n_particles, v_0, curvity, persi
     t = np.arange(0, t_end, dt) # time array
     n, timestep_end = len(t), len(t) #timesteps, time_step end tracks the timesteps for the arrhenius
 
-    # setting parameters -> adjustable to heterogeneous particles: np.random.rand(n_particles, 1) -> can go into one big array
+    # setting parameters -> adjustable to heterogeneous particles: np.random.rand(n_particles, 1) 
     v0 = np.ones((n_particles, 1)) * v_0 #self propulsion speed
     b = np.ones((n_particles,1)) #particle radius #n
     kappa = np.ones((n_particles, 1)) * curvity
@@ -34,7 +32,7 @@ def simulate_system(radius_repulsive_potential, n_particles, v_0, curvity, persi
     trajectories[:,0,:N] = x_0
     trajectories[:,0,N:]= rotation_start
 
-    #sets eperiment up if there is one, overwrites previously set initial conditions
+    #sets experiment up if indicated, overwrites previously set initial conditions
     if mode != None:
         storage_occupancy, params_particles, params_system = u.setup_experiment(mode, params_particles, params_system, trajectories)
 
@@ -42,7 +40,7 @@ def simulate_system(radius_repulsive_potential, n_particles, v_0, curvity, persi
     for i in tqdm(range(n-1)):  # Loop through timesteps
         
         # Position and orientation update with Euler integration (vectorized)
-        cos_theta, sin_theta = np.cos(trajectories[:, i, N]), np.sin(trajectories[:, i, N]) #index N itself  is the orientation
+        cos_theta, sin_theta = np.cos(trajectories[:, i, N]), np.sin(trajectories[:, i, N]) 
         if N ==2:
             e = np.stack([cos_theta, sin_theta], axis=1) #shape e (n_particles, 2)            
         #add extra degree of freedom in orientation for 3D
@@ -56,18 +54,18 @@ def simulate_system(radius_repulsive_potential, n_particles, v_0, curvity, persi
         trajectories[:, i+1, :N] = u.pbc_vec(trajectories[:, i+1, :N], boundary) #apply periodic boundary conditions
         trajectories[:, i+1, N:] = update_orientation(e, persistence_length, particles_vel, dt=dt, curvity = kappa, Dr = Dr, N = N, mode_particles=mode_particles) #returns arctan2
 
-        #Bookkeeping probabilities -> can set this to one simple function
+        #Bookkeeping probabilities 
         if mode == 'Iso_oreo':
             #calculate occupancies repulsive potentials
             for j in range(n_potentials):
                 distances = np.linalg.norm(u.pbc_distance(trajectories[j, i+1, :N], trajectories[:, i+1, :N], boundary), axis=1)
                 storage_occupancy[j] = len(np.where(distances < (b[j] + 1.1))[0])-1 
 
-    # Calculate filling fraction, if there; outside of repulsive potentials
+    # Calculate filling fraction (outside of repulsive potentials)
     space_repulsive_potentials = np.sum((np.pi**(N/2)/math.gamma(N/2 + 1)) * b[:n_potentials]**N) #hypersphere
     free_box_space = boundary**N - space_repulsive_potentials #space where particles live
     space_particles = np.sum((np.pi**(N/2)/math.gamma(N/2 + 1)) * b[n_potentials:]**N) #hypersphere
-    filling_fraction = space_particles/free_box_space # space particles/box_space: box_space = L**2 - repulsive potentials
+    filling_fraction = space_particles/free_box_space 
     
     #saving data
     data_save = 1 #space between stored timesteps
@@ -113,13 +111,13 @@ def compute_pairwise_forces(n_particles, current_positions, radius_particles, st
     radius_particles, stiffness shape (n_particles, 1)
     """ 
     
-    #some bookkeeping and a buffer
+    #some bookkeeping
     f = np.zeros((n_particles, N))
-    epsilon = 1e-10
+    epsilon = 1e-10 #buffer
 
     # Compute pairwise displacement vectors and apply periodic boundaries    
     r1, r2 = current_positions[:, np.newaxis, :], current_positions[np.newaxis, :, :] #shape (n_particles, 1, N), (1, n_particles, N), for broadcasting
-    rij = u.pbc_distance(r1, r2, boundary)  # shape (n_particles, n_particles, N) -> 2D vector, center to center
+    rij = u.pbc_distance(r1, r2, boundary)  # shape (n_particles, n_particles, N) -> center to center, vector
     
     #absolute distances
     rij_abs = np.linalg.norm(rij, axis=2)  # shape (n_particles, n_particles)
@@ -131,12 +129,12 @@ def compute_pairwise_forces(n_particles, current_positions, radius_particles, st
         rij_abs_sub = rij_abs[:n_potentials, n_potentials:]  
         radii_sum = radius_particles[:n_potentials] + radius_particles[n_potentials:].T  # (n_potentials, n_particles-n_potentials)
         
-        #harmonic
+        #harmonic potential
         gamma = np.where(rij_abs_sub <= radii_sum, (stiffness[:n_potentials]/2)*(radii_sum-rij_abs_sub)**2,0.0)#np.exp(stiffness[:n_potentials] * (1 - rij_abs_sub / radii_sum)), 0.0)  # (n_potentials, n_particles-n_potentials)
-
         forces = gamma[:, :, np.newaxis] * rij_sub / (rij_abs_sub[:, :, np.newaxis] + epsilon)  # (n_particles, n_particles-n_potentials, N)
         f[:n_potentials] += np.sum(forces, axis=1)  # sum over the second axis 
         f[n_potentials:] -= np.sum(forces.transpose(1, 0, 2), axis=1) #(n_particles-n_potentials, n_potentials, N)
+    
     else:
         radii_sum = radius_particles + radius_particles.T # shape (n_particles, n_particles)
         gamma = np.where(rij_abs<2**(1/6)*2, 4 * (((radius_particles*2)/rij_abs)**12-((radius_particles*2)/rij_abs)**6)+1, 0.0) #WCA
@@ -168,6 +166,7 @@ def update_orientation(e, persistence_length, particles_vel, dt, curvity, Dr, N,
             #Dr is the tumbling rate alpha, see solon 2016
             poisson = np.random.poisson(Dr, size = (n_particles, N)) 
             e += tumble_angle*poisson * np.sqrt(dt)
+            
         norm = np.linalg.norm(e, axis=1) 
         e /= norm[:, np.newaxis]  
         arctan = np.arctan2(e[:, 1], e[:, 0]) #returns (N-1) orientation DOF, arctan (x2,x1)
@@ -193,7 +192,6 @@ def update_orientation(e, persistence_length, particles_vel, dt, curvity, Dr, N,
         theta = np.arctan2(e[:, 1], e[:, 0]) 
         phi = np.arccos(e[:, 2]) 
         dof = np.stack([theta, phi], axis = 1) #theta, phi
-
     return dof #shape (n_particles, N-1)
 
 
