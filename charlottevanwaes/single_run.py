362937f190a74ce9d593a7ae9c7db4ae76e1643e
Charlotte van Waes <105546462+Crmm2000@users.noreply.github.com>
2025-07-12 01:52:53 +0200

    Update single_run.py

--- charlottevanwaes/single_run.py
+++ charlottevanwaes/single_run.py
@@ -3,25 +3,25 @@ import pickle
 import Visuals as V 
 
 # parameters system
-t_end, dt = 10, 1e-3 #check whether timesteps are sufficient, minimal 1e-3 WCA potential
+t_end, dt = 10, 1e-3 #minimal 1e-3 WCA potential
 L = 100 #boxsize
-N = 2 
+N = 3
 
 # simulation settings
-interacting = False
+interacting = True
 mode = None #or: 'Iso_oreo', 'Arrhenius', for both interacting is set to false
 mode_particles = 'ABP' #or: RTP
 
 # parameters particles/potential
-n_particles = 200 #amount of particles in system 
+n_particles = 500 #amount of particles in system (including potentials)
 v_0 = 10 #self propulsion speed
-curvity = -1
+curvity = -8
 stiffness = 10
-persistence_length = 5
+persistence_length = 10
 
 # parameters potential
 R_small_potential = 5 #size small repulsive potential, for iso oreo, Arrhenius, etc
-stiffness_potential = 1
+stiffness_potential = 10
 
 # run simulation
 data = Engine.simulate_system(R_small_potential, stiffness_potential, n_particles, v_0, curvity, persistence_length, t_end, dt, L, stiffness, N = N, interacting=interacting, mode = mode, mode_particles = mode_particles)
@@ -37,7 +37,7 @@ params = data['Params']
 
 # Visualize, takes some time
 file_name = 'Test'
-frames_visualized = 100 #(higher saves time) =amount of timesteps (stored, see datasave param main code) between each frame
+frames_visualized = 80 #(higher saves time) =amount of timesteps (stored, see datasave param main code) between each frame
 fps = 40 #frames per second
-V.save_fig(trajectories[:,:,:N], trajectories[:,:,N-1].T, params, frames_visualized
-           , fps, f'{file_name}', N = N, live = False, view = None)
+V.save_fig(trajectories[:,:,:N], trajectories[:,:,N].T, data, params, frames_visualized
+           , fps, f'{file_name}', N = N, live = False, view = 'x', mode = mode)
